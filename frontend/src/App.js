73cd8c20362302a982889003c4273ab6cd4a277a
Bruno Nascimento <sputnicker@gmail.com>
2021-09-12 02:21:14 -0300

    Added music support

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,73 @@
 import { useState } from 'react'
 import styled from 'styled-components'
+import * as gb from 'gameboy-sound'
 
 import Screen from './Components/Screen'
+import { tracks } from './Songs/song'
 
 function App() {
-  const [brightness, setBrightness] = useState(100)
+  const [started, setStarted] = useState(false)
+  const [isMusicPlaying, setMusicPlaying] = useState(false)
+  const [brightness, setBrightness] = useState(50)
+
+  function handleStart() {
+    setStarted(true)
+    if (!isMusicPlaying) {
+      gb.allow()
+      gb.playAll(tracks)
+      setMusicPlaying(true)
+    }
+  }
+
   return (
     <Background>
       <Container>
-        <Machine>
+        {!started && <StartButton onClick={() => handleStart()}>Iniciar Pokedex</StartButton>}
+
+        {started && (<><Machine>
           <Screen brightness={brightness}>
             <h1>Pokedex</h1>
           </Screen>
         </Machine>
         <Machine>
           <Screen brightness={brightness}>
+            <div className="hinge1"/>
+            <div className="hinge2"/>
             <h1>Pokedex</h1>
           </Screen>
-        </Machine>
+        </Machine></>)}
       </Container>
     </Background>
   )
 }
 
 export default App
+
+const StartButton = styled.button`
+  box-shadow: inset 0px 1px 0px 0px #cf866c;
+  background: linear-gradient(to bottom, #d0451b 5%, #bc3315 100%);
+  background-color: #d0451b;
+  border-radius: 3px;
+  border: 1px solid #942911;
+  display: inline-block;
+  cursor: pointer;
+  color: #ffffff;
+  font-family: Arial;
+  font-size: 13px;
+  padding: 4vh 4vh;
+  text-decoration: none;
+  text-shadow: 0px 1px 0px #854629;
+  font-family: 'Press Start 2P', cursive;
+
+  &:hover {
+    background: linear-gradient(to bottom, #bc3315 5%, #d0451b 100%);
+    background-color: #bc3315;
+  }
+  &:active {
+    position: relative;
+    top: 1px;
+  }
+`
 
 const Background = styled.div`
   background-color: #000;
@@ -68,12 +112,12 @@ const Container = styled.div`
 
 const Machine = styled.div`
   background-color: #dd4d4d;
-  box-shadow: .5vh 1vh 0vh #9e3838;
+  box-shadow: 0.5vh 1vh 0vh #9e3838;
   width: 100%;
   height: 90vh;
   border-radius: 2vh;
   padding: 1vh;
   display: flex;
   flex-direction: column;
-  border: .3vh solid rgba(255, 255, 255, 0.10);
+  border: 0.3vh solid rgba(255, 255, 255, 0.1);
 `
